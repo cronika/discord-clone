500a285a71aab426b1e5cb0f1a749157be9fb554
cronika <torunedo.sawamura@gmail.com>
2024-09-20 20:43:10 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import React, { useEffect } from 'react';
-import './App.scss';
-import Sidebar from './Compornents/Sidebar/Sidebar';
-import Chat from './Compornents/Chat/Chat';
-import Login from './login/Login';
-import { useAppDispatch, useAppSelector } from './app/hooks';
-import { auth } from './firebase';
-import { login, logout } from './features/useSlice';
-import { ErrorBoundary } from 'react-error-boundary';
+import React, { useEffect } from "react";
+import "./App.scss";
+import Sidebar from "./Compornents/Sidebar/Sidebar";
+import Chat from "./Compornents/Chat/Chat";
+import Login from "./login/Login";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { auth } from "./firebase";
+import { login, logout } from "./features/useSlice";
+import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallBack } from "./utils/ErrorFallBack";
 
 function App() {
-
   const user = useAppSelector((state) => state.user.user);
   // const user = null;
   // console.log(user);
 
   const dispatch = useAppDispatch();
-
+  //
   useEffect(() => {
     auth.onAuthStateChanged((loginUser) => {
       console.log(loginUser);
-      if(loginUser) {
+      if (loginUser) {
         dispatch(
           login({
-           uid: loginUser.uid,
-           photo: loginUser.photoURL,
-           email: loginUser.email,
-           displayName: loginUser.displayName,
-         })
-       );
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        );
       } else {
         dispatch(logout());
       }
@@ -38,15 +37,15 @@ function App() {
   return (
     <div className="App">
       {user ? (
-        <> 
-        <ErrorBoundary FallbackComponent={ErrorFallBack}>
-          <Sidebar/>
-        </ErrorBoundary>
-        <Chat/>
+        <>
+          <ErrorBoundary FallbackComponent={ErrorFallBack}>
+            <Sidebar />
+          </ErrorBoundary>
+          <Chat />
         </>
       ) : (
         <>
-         <Login/>
+          <Login />
         </>
       )}
     </div>
--- src/Compornents/Chat/Chat.tsx
+++ src/Compornents/Chat/Chat.tsx
@@ -66,7 +66,7 @@ const Chat = () => {
             user={message.user}
           />
         ))}
-        ;
+
         {/* <ChatMessage />
         <ChatMessage />S
         <ChatMessage />
